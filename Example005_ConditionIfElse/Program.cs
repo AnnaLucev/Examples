cdb020c4b743dada9bfd864e7b9a4f691e715234
Anna Lutsevich <alucevich@mail.ru>
2023-02-10 18:50:45 +0700

    Add folder Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿// Приветствие пользователя - любимчика
+
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")               // переводит весь текст в нижний регистр
+{
+   Console.WriteLine("Ура! Это же МАША!"); 
+}
+else 
+{
+    Console.Write("Привет,  "); 
+    Console.WriteLine(username); 
+}
+
