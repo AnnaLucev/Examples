d1b10ebdd1c8da6c3aa519657a0a586def95d781
Anna Lutsevich <alucevich@mail.ru>
2023-02-10 17:46:42 +0700

    Add folder Example004_Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа нахождения частного
+
+int numberA = 12;
+int numberB = 5;
+int result = numberA / numberB;
+
+Console.WriteLine(result);
+
+double numberA1 = 12;
+double numberB1 = 5;
+double result1 = numberA1 / numberB1;
+
+Console.WriteLine(result1);
