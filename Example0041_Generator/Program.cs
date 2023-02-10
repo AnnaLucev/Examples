f527f504e84b9725a4e56a532bbafff862bd134c
Anna Lutsevich <alucevich@mail.ru>
2023-02-10 17:52:51 +0700

    Add folder Example0041_Generator

---
+++ Example0041_Generator/Program.cs
@@ -0,0 +1,9 @@
+﻿// Находим частное с использованием генератора случайных чисел
+
+int numberA = new Random().Next(1,10); // 1-9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA / numberB;
+
+Console.WriteLine(result);
